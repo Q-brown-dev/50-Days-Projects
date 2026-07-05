98b440fdd547b6516c6f3b3f80167ed15366b965
unknown <anyikwabruno1@.com>
2026-07-05 15:21:41 -0700

    Update this folder

---
+++ day4/js/script.js
@@ -0,0 +1,22 @@
+const toggles = document.querySelectorAll('.toggle');
+const good = document.querySelector('#good');
+const cheap = document.querySelector('#cheap');
+const fast = document.querySelector('#fast');
+
+
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)));
+
+function doTheTrick(theClickOne) {
+    if (good === checked && cheap === checked && fast === checked) {
+        if (good === theClickOne) {
+            fast.checked = false;
+        } 
+        else if(cheap === theClickOne) {
+            good.checked = false;
+        }
+        else if(fast === theClickOne) {
+            cheap.checked = false;
+        }
+    }
+}
+
