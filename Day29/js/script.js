509ce13b262fe05df5dbb3d6ba4e10b004c63c28
unknown <anyikwabruno1@.com>
2026-07-05 15:27:42 -0700

    Update this folder

---
+++ Day29/js/script.js
@@ -0,0 +1,118 @@
+function clickMeBtn() {
+    // circles
+    const circle1 = document.getElementById("circle-1");
+    const circle2 = document.getElementById("circle-2")
+    const circle3 = document.getElementById("circle-3")
+    const circle4 = document.getElementById("circle-4")
+    const circle5 = document.getElementById("circle-5")
+    const circle6 = document.getElementById("circle-6")
+    const circle7 = document.getElementById("circle-7")
+    const circle8 = document.getElementById("circle-8")
+    const circle9 = document.getElementById("circle-9")
+    const circle10 = document.getElementById("circle-10")
+    const circle11 = document.getElementById("circle-11")
+    const circle12 = document.getElementById("circle-12")
+    const circle13 = document.getElementById("circle-13")
+    const circle14 = document.getElementById("circle-14")
+    const circle15 = document.getElementById("circle-15")
+    const circle16 = document.getElementById("circle-16")
+    const circle17 = document.getElementById("circle-17")
+    const circle18 = document.getElementById("circle-18")
+    const circle19 = document.getElementById("circle-19")
+    circle1.classList.add("active")
+    circle2.classList.add("active")
+    circle3.classList.add("active")
+    circle4.classList.add("active")
+    circle5.classList.add("active")
+    circle6.classList.add("active")
+    circle7.classList.add("active")
+    circle8.classList.add("active")
+    circle9.classList.add("active")
+    circle10.classList.add("active")
+    circle11.classList.add("active")
+    circle12.classList.add("active")
+    circle13.classList.add("active")
+    circle14.classList.add("active")
+    circle15.classList.add("active")
+    circle16.classList.add("active")
+    circle17.classList.add("active")
+    circle18.classList.add("active")
+    circle19.classList.add("active")
+    setTimeout(() => {
+        circle19.classList.remove("active")
+        circle19.style.transition = "1s";
+    }, 5000);
+    setTimeout(() => {
+        circle18.classList.remove("active")
+        circle18.style.transition = "2s";
+    }, 6000);
+    setTimeout(() => {
+        circle17.classList.remove("active")
+        circle17.style.transition = "3s";
+    }, 7000);
+    setTimeout(() => {
+        circle16.classList.remove("active")
+        circle16.style.transition = "4s";
+    }, 8000);
+    setTimeout(() => {
+        circle15.classList.remove("active")
+        circle15.style.transition = "5s";
+    }, 9000);
+    setTimeout(() => {
+        circle14.classList.remove("active")
+        circle14.style.transition = "6s";
+    }, 10000);
+    setTimeout(() => {
+        circle13.classList.remove("active")
+        circle13.style.transition = "7s";
+    }, 11000);
+    setTimeout(() => {
+        circle12.classList.remove("active")
+        circle12.style.transition = "8s";
+    }, 12000);
+    setTimeout(() => {
+        circle11.classList.remove("active")
+        circle11.style.transition = "9s";
+    }, 13000);
+    setTimeout(() => {
+        circle10.classList.remove("active")
+        circle10.style.transition = "10s";
+    }, 14000);
+    setTimeout(() => {
+        circle9.classList.remove("active")
+        circle9.style.transition = "11s";
+    }, 15000);
+    setTimeout(() => {
+        circle8.classList.remove("active")
+        circle8.style.transition = "12s";
+    }, 16000);
+    setTimeout(() => {
+        circle7.classList.remove("active")
+        circle7.style.transition = "13s";
+    }, 17000);
+    setTimeout(() => {
+        circle6.classList.remove("active")
+        circle6.style.transition = "14s";
+    }, 18000);
+    setTimeout(() => {
+        circle5.classList.remove("active")
+        circle5.style.transition = "15s";
+    }, 19000);
+    setTimeout(() => {
+        circle4.classList.remove("active")
+        circle4.style.transition = "16s";
+    }, 20000);
+    setTimeout(() => {
+        circle3.classList.remove("active")
+        circle3.style.transition = "17s";
+    }, 21000);
+    setTimeout(() => {
+        circle2.classList.remove("active")
+        circle2.style.transition = "18s";
+    }, 22000);
+    setTimeout(() => {
+        circle1.classList.remove("active")
+        circle1.style.transition = "19s";
+    }, 23000);
+}
+
