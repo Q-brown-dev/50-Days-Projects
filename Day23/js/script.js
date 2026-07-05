3f8a82fad593a75a206e9bb87ccbaa52217bfbc4
unknown <anyikwabruno1@.com>
2026-07-05 15:26:30 -0700

    Update this folder

---
+++ Day23/js/script.js
@@ -0,0 +1,19 @@
+    
+const btn = document.getElementById("backToTop");
+
+// Show button when scrolling down
+window.onscroll = function () {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+// Scroll to top when clicked
+btn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+};
